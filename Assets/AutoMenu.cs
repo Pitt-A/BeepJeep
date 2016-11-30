90c4b5d38c62b3005c52751d90c807169b7d1125
n8-webb <niallfcwebb@gmail.com>
2016-11-30 14:23:05 +0000

    BOISH GOT SOME MENU

---
+++ Assets/AutoMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoMenu : MonoBehaviour {
+    public GameObject car;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        car.transform.Translate(Vector2.right * 2 * Time.deltaTime);
+    }
+}
